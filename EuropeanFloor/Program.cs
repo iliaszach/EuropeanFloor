d24bc9c8f2d2c80f5c1aa727f6be20680c5fbf76
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-07-01 15:01:33 +0300

    Add project files.

---
+++ EuropeanFloor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuropeanFloor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int GetRealFloor(int n)
+        {
+
+            return n <= 0 ? n : n < 13 ? n - 1 : n - 2;
+        }
+    }
+}
